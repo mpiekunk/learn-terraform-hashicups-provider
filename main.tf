1e2e639f9dd566bdd9030e6c98a2eff2b01707e3
mpiekunk <mpiekunk@users.noreply.github.com>
2021-05-19 15:54:11 -0400

    read coffee ingredients

--- main.tf
+++ main.tf
@@ -30,3 +30,11 @@ resource "hashicups_order" "edu" {
 output "edu_order" {
   value = hashicups_order.edu
 }
+
+data "hashicups_ingredients" "first_coffee" {
+  coffee_id = hashicups_order.edu.items[0].coffee[0].id
+}
+
+output "first_coffee_ingredients" {
+  value = data.hashicups_ingredients.first_coffee
+}
